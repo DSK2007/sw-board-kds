c655a9138266dc0af7d3bfdc17fb2aedeaefac83
DSK2007 <202112045@office.induk.ac.kr>
2023-06-14 04:34:20 +0900

    20230613-14 Night Commit

--- src/main/java/idusw/springboot/repository/ReplyRepository.java
+++ src/main/java/idusw/springboot/repository/ReplyRepository.java
@@ -5,9 +5,11 @@ import idusw.springboot.entity.ReplyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Long> {
     @Modifying
     @Query("delete from ReplyEntity r where r.board.bno = :bno")
